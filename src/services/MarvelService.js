a5b0840cec5693d92eb15bf4e7f38b879bd65b89
bogdansolomich <bogdansolomich@gmail.com>
2024-10-23 21:00:34 +0300

    Отпр

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,5 +1,6 @@
 class MarvelService {
   _apiBase = "https://gateway.marvel.com:443/v1/public/";
+  // ЗДЕСЬ БУДЕТ ВАШ КЛЮЧ, ЭТОТ КЛЮЧ МОЖЕТ НЕ РАБОТАТЬ
   _apiKey = "apikey=c5d6fc8b83116d92ed468ce36bac6c62";
 
   getResource = async (url) => {
@@ -12,10 +13,11 @@ class MarvelService {
     return await res.json();
   };
 
-  getAllCharacters = () => {
-    return this.getResource(
+  getAllCharacters = async () => {
+    const res = await this.getResource(
       `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
     );
+    return res.data.results.map(this._transformCharacter);
   };
 
   getCharacter = async (id) => {
@@ -27,11 +29,15 @@ class MarvelService {
 
   _transformCharacter = (char) => {
     return {
+      id: char.id,
       name: char.name,
-      description: char.description,
+      description: char.description
+        ? `${char.description.slice(0, 210)}...`
+        : "There is no description for this character",
       thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
       homepage: char.urls[0].url,
       wiki: char.urls[1].url,
+      comics: char.comics.items,
     };
   };
 }
