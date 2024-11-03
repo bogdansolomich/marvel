f31de59bc022811e830951b070d8dc135a89b3d6
bogdansolomich <bogdansolomich@gmail.com>
2024-11-03 14:06:02 +0200

    Update AppHeader.js

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -4,18 +4,18 @@ const AppHeader = () => {
   return (
     <header className="app__header">
       <h1 className="app__title">
-        <a href="/">
+        <a href="https://bogdansolomich.github.io/marvel/">
           <span>Marvel</span> information portal
         </a>
       </h1>
       <nav className="app__menu">
         <ul>
           <li>
-            <a href="/">Characters</a>
+            <a href="https://bogdansolomich.github.io/marvel/">Characters</a>
           </li>
           /
           <li>
-            <a href="/">Comics</a>
+            <a href="https://bogdansolomich.github.io/marvel/">Comics</a>
           </li>
         </ul>
       </nav>
